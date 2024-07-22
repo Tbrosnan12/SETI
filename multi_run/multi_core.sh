ba8bb94ad1ac13d0c9e02d7509145034a289ab3b
Tbrosnan12 <tbrosnan@tcd.ie>
2024-07-22 01:16:43 +0100

    fully add multi core

--- multi_run/multi_core.sh
+++ multi_run/multi_core.sh
@@ -5,6 +5,19 @@ if [ -d "iter0" ]; then
   echo "removing previous data"
 fi
 
-for i in $(seq 0 1 $1); do
+usage() {
+    echo "Usage: $0  <start cpu No.> <end cpu No.>"
+    exit 1
+}
+
+if [ "$#" -lt 2 ]; then
+    echo "Error: Invalid number of arguments."
+    usage
+fi
+
+start=$1
+end=$2
+
+for i in $(seq  $start 1 $end); do
    csh schedtool.csh $i &
 done
