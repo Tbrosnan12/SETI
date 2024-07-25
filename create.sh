46343aac0a0a9c9ae987ffaf11949922816b662c
Tbrosnan12 <tbrosnan@tcd.ie>
2024-07-25 16:57:30 +0100

    updates

--- create.sh
+++ create.sh
@@ -26,11 +26,12 @@ cd output_files
 touch injected_snr.txt
 
 # Create the pulses
-python3 ../simscript_thomas.py --dm_start ${DM_start} --dm ${DM_end} --step ${DM_step} --sig_start ${width_start} --sig_step ${width_step} --sig ${width_end} -N 1 -A $amp -s 5000
+python3 ../simscript_thomas.py  --dm_start ${DM_start} --dm ${DM_end} --step ${DM_step} --sig_start ${width_start} --sig_step ${width_step} --sig ${width_end} -N 1000 -A $amp -s 5000
 if [ $? -ne 0 ]; then
    echo "Error: Failed to run simscript_thomas.py"
    exit 1
 fi
+
 echo "Inverting filterbanks"
 for width1 in $(seq $width_start $width_step $width_end); do
 	for DM1 in $(seq $DM_start $DM_step $DM_end); do
@@ -38,11 +39,11 @@ for width1 in $(seq $width_start $width_step $width_end); do
       width=$(python3 ../custom_round.py $width1 2)
       DM=$(python3 ../custom_round.py $DM1 0)
  
-      #python3 ../invert.py test_single_dm${DM}_width${width}.fil | grep "nothing"
-      #rm test_single_dm${DM}_width${width}.fil
+      python3 ../invert.py test_single_dm${DM}_width${width}.fil | grep "nothing"
+      rm test_single_dm${DM}_width${width}.fil
    done
 done 
-   
+
 touch ranges.txt
 echo $DM_start >> ranges.txt
 echo $DM_end >> ranges.txt
