7869f5641e03da902234953308937d54a37bee4d
Tbrosnan12 <tbrosnan@tcd.ie>
2024-07-25 14:09:19 +0100

    updates

--- transientx.sh
+++ transientx.sh
@@ -45,7 +45,7 @@ else
 
       mkdir "${model}_output/${file}.${model}"  
       cd ${model}_output/${file}.${model}                                        #dedispersing and searching 
-      transientx_fil -f ../../${file} --dms ${DM} --ddm 0 --ndm 1 --thre 7 --saveimage --threMask 100 --threPatch 100 --thre 20
+      transientx_fil -f ../../${file} --dms ${DM} --ddm 0 --ndm 1 --thre 20 --saveimage --threMask 100 --threPatch 100 
 
 
       echo "searching $file"
