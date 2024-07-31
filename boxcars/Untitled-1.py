7950e54e00518f338e961aad4a1a0d4e1b6c9439
Tbrosnan12 <tbrosnan@tcd.ie>
2024-07-31 16:17:08 +0100

    added stuff for boxcar plots

---
+++ boxcars/Untitled-1.py
@@ -0,0 +1,21 @@
+
+import matplotlib.pyplot as plt
+import sys
+from matplotlib import cm
+import numpy as np
+
+def read_file(filename):
+    with open(filename, 'r') as file:
+        array = []
+        for line in file:
+            row = line.strip().split()
+            row = [float(num) for num in row]
+            array.append(row)
+    return array
+
+snr=np.array(read_file(sys.argv[1]))
+xaxis=np.zeros(round(len(snr)))
+for i in range(round(len(snr))):
+    xaxis[i]=i
+
+plt.plot(xaxis,snr)
