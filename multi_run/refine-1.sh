94098ef32f5bd55c4ef4d4bc66118abdc08a464e
Tbrosnan12 <tbrosnan@tcd.ie>
2024-08-12 21:35:03 +0100

    updates

--- multi_run/refine-1.sh
+++ multi_run/refine-1.sh
@@ -2,11 +2,11 @@
 model=$1
 
 cd output_files
-mv injected_snr.txt ../
+cp injected_snr.txt ../
 if [ -f "${model}_boxcar.png" ]; then
-    mv ${model}_boxcar.png ../
+    cp ${model}_boxcar.png ../
 fi
-mv ${model}_output/${model}.txt ../
+cp ${model}_output/${model}.txt ../
 cd ..
 rm -r output_files
 rm *py && rm *sh && rm -r python
